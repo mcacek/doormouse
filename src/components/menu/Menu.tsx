8a1e72b457f4046bc20bb9745e2a2540ae02b968
Michael Cacek <michael.cacek@gmail.com>
2022-02-03 12:46:02 -0600

    feat: make menu non-functional
    Remove menu functionality but leave placeholder for display purposes.

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,28 +1,21 @@
 import * as React from 'react';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { FiChevronDown } from 'react-icons/fi';
 
 interface MenuProps {
 	label: string;
-	children: React.ReactNode;
 }
 
-function Menu({ label, children }: MenuProps) {
-	const [isOpen, setIsOpen] = React.useState(false);
-
+function Menu({ label }: MenuProps) {
 	const toggleMenu = React.useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
 		event.preventDefault();
-		setIsOpen((current) => !current);
 	}, []);
-
-	const Indicator = isOpen ? FiChevronUp : FiChevronDown;
 
 	return (
 		<>
 			<div className="text-left">
 				<button className="uppercase" onClick={toggleMenu}>
-					{label} <Indicator className="inline-block" />
+					{label} <FiChevronDown className="inline-block" />
 				</button>
-				{isOpen && <ul className="absolute bg-white px-4 w-40 right-0">{children}</ul>}
 			</div>
 		</>
 	);
